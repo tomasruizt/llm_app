ec957ef73839d086cec47c0479983c133513c3e3
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2024-10-09 15:21:58 +0200

    Test that whisper can take mp4 as well

--- tests/test_whisper.py
+++ tests/test_whisper.py
@@ -5,8 +5,15 @@ from tests.helpers import is_ci, test_file_path
 
 @pytest.mark.skipif(condition=is_ci(), reason="No GPU in CI")
 def test_transcription():
-    file = str(test_file_path(name="some-audio.flac"))  # Librispeech sample 2
+    audio_file = str(test_file_path(name="some-audio.flac"))  # Librispeech sample 2
     expected_transcription = "before he had time to answer a much encumbered vera burst into the room with the question i say can i leave these here these were a small black pig and a lusty specimen of black-red game-cock"
     model = Whisper()
-    actual_transcription: str = model.transcribe_file(file)
+    actual_transcription: str = model.transcribe_file(audio_file)
     assert actual_transcription == expected_transcription
+
+    video_file = str(test_file_path("video.mp4"))
+    expected_fragment = (
+        "Die Unionsparteien oder deren Politiker sind heute wichtige Offiziere"
+    )
+    transcription = model.transcribe_file(video_file)
+    assert expected_fragment in transcription
