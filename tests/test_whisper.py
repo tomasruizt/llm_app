d502c7a4b69f0893949ee74121b842ce8eaa40f8
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2024-10-09 11:54:09 +0200

    Skip Whisper unit test in CI due to no GPU

--- tests/test_whisper.py
+++ tests/test_whisper.py
@@ -1,7 +1,9 @@
 from llmlib.whisper import Whisper
-from tests.helpers import test_file_path
+import pytest
+from tests.helpers import is_ci, test_file_path
 
 
+@pytest.mark.skipif(condition=is_ci(), reason="No GPU in CI")
 def test_transcription():
     file = str(test_file_path(name="some-audio.flac"))  # Librispeech sample 2
     expected_transcription = "before he had time to answer a much encumbered vera burst into the room with the question i say can i leave these here these were a small black pig and a lusty specimen of black-red game-cock"
