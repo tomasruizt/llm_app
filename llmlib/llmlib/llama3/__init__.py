de62c13cefc637ecb6a42d33bb5c51632fa49696
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2024-12-22 18:46:57 +0100

    Remove non-working LLama 70B quantized model

--- llmlib/llmlib/llama3/__init__.py
+++ llmlib/llmlib/llama3/__init__.py
@@ -1,4 +1,3 @@
-from .llama3_vision_70b_quantized import LLama3Vision70BQuantized
 from .llama3_vision_8b import LLama3Vision8B
 
-__all__ = ["LLama3Vision70BQuantized", "LLama3Vision8B"]
+__all__ = ["LLama3Vision8B"]
--- tests/test_llama3.py
+++ tests/test_llama3.py
@@ -1,7 +1,6 @@
 from llmlib.base_llm import LLM
 import pytest
 
-from llmlib.llama3.llama3_vision_70b_quantized import LLama3Vision70BQuantized
 from llmlib.llama3.llama3_vision_8b import LLama3Vision8B
 
 from .helpers import (
@@ -16,11 +15,3 @@ def test_llama_8b():
     model: LLM = LLama3Vision8B()
     assert_model_knows_capital_of_france(model)
     assert_model_recognizes_pyramid_in_image(model)
-
-
-@pytest.mark.skipif(condition=is_ci(), reason="No GPU in CI")
-def test_llama_70b_quantized():
-    model: LLM = LLama3Vision70BQuantized()
-    assert_model_knows_capital_of_france(model)
-    # model cannot recognize mona lisa yet
-    # assert_model_recognized_mona_lisa_in_image(model)
