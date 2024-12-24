f2f4d5296277daca40a7a632ff2202fc4d8fdc01
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2024-12-24 01:34:24 +0100

    Set InternVL as default model

--- llmlib/llmlib/runtime.py
+++ llmlib/llmlib/runtime.py
@@ -12,11 +12,11 @@ from .phi3.phi3 import Phi3Vision
 def filled_model_registry() -> ModelRegistry:
     return ModelRegistry(
         models=[
-            ModelEntry.from_cls_with_id(Phi3Vision),
+            ModelEntry.from_cls_with_id(InternVL),
             ModelEntry.from_cls_with_id(MiniCPM),
+            ModelEntry.from_cls_with_id(Phi3Vision),
             ModelEntry.from_cls_with_id(LLama3Vision8B),
             ModelEntry.from_cls_with_id(PaliGemma2),
-            ModelEntry.from_cls_with_id(InternVL),
             *model_entries_from_mult_ids(OpenAIModel),
             *model_entries_from_mult_ids(GeminiAPI),
         ]
