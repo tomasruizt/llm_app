5923a276e4c1b6b06376f81e3b42e2163511b176
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2024-12-28 15:57:49 +0100

    Enable multi-turn chat with Apollo 7B

--- llmlib/llmlib/replicate_api.py
+++ llmlib/llmlib/replicate_api.py
@@ -1,24 +1,42 @@
 from io import BytesIO
+import json
 from logging import getLogger
 from pathlib import Path
-from llmlib.base_llm import LLM
+from typing import Literal, TypedDict
+from llmlib.base_llm import LLM, Message
 import replicate
 
 logger = getLogger(__name__)
 
 
+class Msg(TypedDict):
+    role: Literal["user", "assistant"]
+    content: str
+
+
 class Apollo7B(LLM):
     model_id = "replicate/lucataco/apollo-7b"
-    model_id_full = "lucataco/apollo-7b:e282f76d0451b759128be3e8bccfe5ded8f521f4a7d705883e92f837e563f575"
+    model_id_full = "tomasruizt/apollo-7b-multiturn:1120d3a705916a77c094d3bfc180089f6a4e46f840218bd1663b00b889da2951"
 
     def video_prompt(self, video: Path | BytesIO, prompt: str) -> str:
         logger.info("Calling Replicate API with model %s", self.model_id)
+        messages = [{"role": "user", "content": prompt}]
+        return self.call_api(video=video, messages=messages)
+
+    def complete_msgs2(self, msgs: list[Message]) -> str:
+        fst_msg = msgs[0]
+        if not fst_msg.has_video():
+            raise ValueError("Apollo7B only supports video input")
+        messages = [Msg(role=msg.role, content=msg.msg) for msg in msgs]
+        return self.call_api(video=fst_msg.video, messages=messages)
+
+    def call_api(self, video: Path | BytesIO, messages: list[Msg]) -> str:
         output = replicate.run(
             self.model_id_full,
             input={
                 "top_p": 0.7,
                 "video": video,
-                "prompt": prompt,
+                "messages": json.dumps(messages),
                 "temperature": 0.4,
                 "max_new_tokens": 512,
             },
