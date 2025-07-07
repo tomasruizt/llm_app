0d3bfce4fd7b1691c4b47697421ded0ed1c2348f
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2025-07-07 11:33:13 +0200

    Add vLLM Whisper as proof-of-concept

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 .PHONY: vllm-server
+.PHONY: whisper-vllm
 
 vllm-server:
 	python -m vllm.entrypoints.openai.api_server \
@@ -14,3 +15,13 @@ vllm-server:
 		--host 127.0.0.1 \
 		--gpu-memory-utilization 0.8 \
 		--enforce-eager
+
+whisper-vllm:
+	VLLM_USE_V1=0 python -m vllm.entrypoints.openai.api_server \
+		--model openai/whisper-large-v3-turbo \
+		--task transcription \
+		--allowed-local-media-path=/ \
+		--limit-mm-per-prompt "audio=1" \
+		--disable-log-requests \
+		--host 127.0.0.1 \
+		--port 9000 \
--- tests/test_whisper.py
+++ tests/test_whisper.py
@@ -6,7 +6,7 @@ import json
 
 @pytest.fixture(scope="module")
 def model() -> Whisper:
-    return Whisper()
+    return Whisper(use_flash_attention=False)
 
 
 @pytest.mark.skipif(condition=is_ci(), reason="No GPU in CI")
@@ -40,7 +40,7 @@ def test_video_transcription_with_timestamps(model: Whisper, snapshot):
 def test_translation(model: Whisper):
     german_video = file_for_test("video.mp4")
     translation: str = model.transcribe_file(german_video, translate=True)
-    assert "The parties and their politicians" in translation
+    assert "it is necessary to be a corrective in this country" in translation
 
 
 @pytest.mark.skipif(condition=is_ci(), reason="No GPU in CI")
@@ -55,3 +55,26 @@ def test_multilingual_transcription(model: Whisper):
     file = file_for_test("multilingual-snippets.mp4")
     transcription: str = model.transcribe_file(file)
     assert isinstance(transcription, str)
+
+
+def test_whisper_vllm():
+    from openai import OpenAI
+
+    client = OpenAI(
+        api_key="EMPTY",
+        base_url="http://localhost:9000/v1",
+    )
+    fpath = file_for_test("some-audio.flac")
+    assert fpath.exists()
+
+    expected_transcription = "before he had time to answer a much encumbered vera burst into the room with the question i say can i leave these here these were a small black pig and a lusty specimen of black-red game-cock"
+
+    with open(str(fpath), "rb") as f:
+        transcription = client.audio.transcriptions.create(
+            file=f,
+            model="openai/whisper-large-v3-turbo",
+            language="en",
+            response_format="text",
+            temperature=0.0,
+        )
+    assert expected_transcription in transcription
